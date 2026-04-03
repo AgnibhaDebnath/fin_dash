25298cd30875a9f541508d36f55c0c879e40d6bf
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-04-03 13:13:48 +0530

    refactor : extract Chart data into ChartsData.js for cleaner component

---
+++ src/Data/ChartsData.js
@@ -0,0 +1,58 @@
+export const monthly_enpense = [
+        {
+            month: "Jan",
+            expense: "5000"
+        },
+        {
+            month: "Fab",
+            expense: "7000"
+        },
+        {
+            month: "Mar",
+            expense: "10000"
+        },
+        {
+            month: "Apr",
+            expense: "3000"
+        },
+        {
+            month: "May",
+            expense: "5000"
+        },
+        {
+            month: "Jun",
+            expense: "9354"
+        },
+        {
+            month: "Jul",
+            expense: "4354"
+        },
+        {
+            month: "Aug",
+            expense: "12000"
+        },
+        {
+            month: "Sep",
+            expense: "6491"
+        },
+        {
+            month: "Oct",
+            expense: "8430"
+        },
+        {
+            month: "Nov",
+            expense: "7559"
+        },
+        {
+            month: "Dec",
+            expense: "15000"
+        },
+]
+    
+ export  const monthly_expense_components = [
+        { name: "Food", value: 3000 },
+        { name: "Shopping", value: 2500 },
+        { name: "Travel", value: 1000 },
+        { name: "Medical", value: 1000 }
+];
+export const COLORS = ["#3b82f6", "#ef4444", "#22c55e", "#f59e0b"];
