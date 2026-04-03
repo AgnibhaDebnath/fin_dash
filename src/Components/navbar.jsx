6c6dea5218496e9cc635ff570c67bfc123fd65c6
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-04-04 02:39:14 +0530

    improve the ui

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -5,7 +5,7 @@ import { FaUser, FaUserShield } from "react-icons/fa";
 const Navbar = ({ role, setRole, activeTab, setIsSidebarOpen }) => {
 
     return (
-        <nav className="px-4 md:px-5 py-4 flex flex-row justify-between bg-white shadow-2xl ">
+        <nav className="px-4 md:px-5 py-6 flex flex-row justify-between bg-white shadow-xl fixed top-0 w-full z-50">
             {activeTab == "dashboard" && <h1 className="font-bold text-3xl pb-1">
                 <button onClick={() => setIsSidebarOpen(true)} className="cursor-pointer min-[768px]:hidden">
                     <LuAlignJustify size={25} className="mt-3" />
@@ -19,8 +19,8 @@ const Navbar = ({ role, setRole, activeTab, setIsSidebarOpen }) => {
                 <span className="ml-5">Transactions</span>
             </h1>}
             <div className="relative hidden md:block">
-                <FaSearch className="absolute top-2 left-3" />
-                <input type="text" placeholder="Search..." className="border-2 rounded-full pl-8 pr-2 pb-1 text-justify hidden md:block" />
+                <FaSearch className="absolute top-2.5 left-3" />
+                <input type="text" placeholder="Search..." className="border-2 rounded-full pl-8 pr-2 py-1 text-justify hidden md:block " />
             </div>
             <div className=" hidden md:block ">
                 <button
