fb062bbc0b51dfc1e3a799ec172f6cc1476f4245
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-04-04 02:30:22 +0530

    Add icons, improve ui and connect to transaction data

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,33 +1,24 @@
-const Cards = () => {
-    const cards_data = [{
-        id: 1,
-        title: "Total Income",
-        value: "₹5000",
-        color: "text-green-600"
-    },
-    {
-        id: 2,
-        title: "Expenses",
-        value: "₹2000",
-        color: "text-red-500"
-    },
-    {
-        id: 3,
-        title: "Balance",
-        value: "₹3000",
-        color: "text-blue-500"
-    }]
+import { FaArrowUp } from "react-icons/fa";
+import { FaArrowDown } from "react-icons/fa";
+import { FaBalanceScale } from "react-icons/fa";
+import { cards_data } from "../Data/CardsData";
+const Cards = ({ total_income, total_expense }) => {
+
 
     return (
         <>
-            <div className="w-full min-[1215px]:h-1/5 h-1/2 flex flex-col items-center mt-2.5">
+            <div className="w-full min-[1215px]:h-1/5 h-2/5 flex flex-col items-center mt-24 px-4 sm:pl-14 sm:pr-4">
                 <h1 className="text-3xl font-bold text-gray-700">Overview</h1>
-                <section className="grid grid-cols-1 min-[1215px]:grid-cols-3 min-[667px]:grid-cols-2 gap-4    justify-items-center w-full pt-8 px-8 h-full ">
 
-                    {cards_data.map((card_data) => (<article key={card_data.id} className="bg-white p-4 rounded-xl shadow-xl hover:shadow-2xl transition min-[667px]:h-4/5 min-[1215px]:h-full w-4/5 min-[667px]:w-full">
-                        <h2 className="text-gray-600">{card_data.title}</h2>
-                        <p className={`text-xl font-bold ${card_data.color}`}>{card_data.value}</p>
-                    </article>))}
+                <section className="grid grid-cols-1 min-[1215px]:grid-cols-3 min-[667px]:grid-cols-2 gap-x-4 h-full justify-items-center w-full  pt-3 gap-y-2">
+
+                    {cards_data.map((card_data) => (
+                        <article key={card_data.id} className="bg-white p-4 rounded-xl shadow-md hover:shadow-lg transition h-full w-4/5 min-[667px]:w-full">
+                            <h2 className="text-gray-600"><span className="">{card_data.id == 1 ? <FaArrowUp className="inline mb-1 mr-1 text-green-500" /> : card_data.id == 2 ? <FaArrowDown className="inline mb-1 mr-1 text-red-500" /> : <FaBalanceScale className="inline mb-1 mr-1 text-blue-500" />}</span>{card_data.title}</h2>
+                            {card_data.id == 1 && <p className={`text-xl font-bold ml-4 ${card_data.color}`}>₹{total_income}</p>}
+                            {card_data.id == 2 && <p className={`text-xl font-bold ml-4 ${card_data.color}`}>₹{total_expense}</p>}
+                            {card_data.id == 3 && <p className={`text-xl font-bold ml-4 ${card_data.color}`}>₹{total_income - total_expense}</p>}
+                        </article>))}
                 </section>
             </div>
         </>
