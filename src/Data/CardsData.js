51a2710edfd8532c5067566cfbe468796a573506
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-04-05 16:31:24 +0530

    refactor: Remove value key from Cards_data

--- src/Data/CardsData.js
+++ src/Data/CardsData.js
@@ -1,18 +1,15 @@
     export const cards_data = [{
         id: 1,
         title: "Total Income",
-        value: "₹5000",
         color: "text-green-600"
     },
     {
         id: 2,
         title: "Expenses",
-        value: "₹2000",
         color: "text-red-500"
     },
     {
         id: 3,
         title: "Balance",
-        value: "₹3000",
         color: "text-blue-500"
     }]
