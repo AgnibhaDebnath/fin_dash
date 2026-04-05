7d8b9f128577fa7a2d89864776c98a1850007339
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-04-05 16:17:51 +0530

    feat:Handle empty state and update ui for empty state

--- src/Components/charts.jsx
+++ src/Components/charts.jsx
@@ -1,17 +1,18 @@
 import { LineChart, XAxis, YAxis, CartesianGrid, Line, Tooltip, PieChart, Pie, Sector, ResponsiveContainer } from "recharts"
-import { monthly_enpense, COLORS } from "../Data/ChartsData";
-const Charts = ({ expense_category }) => {
+import { COLORS } from "../Data/ChartsData";
+import { FaChartLine, FaChartPie } from "react-icons/fa";
+const Charts = ({ expense_category, monthly_expense_data }) => {
 
     const isMobile = window.innerWidth < 640;
 
     return (
         <section className="mt-6 flex flex-col items-center justify-evenly min-[1250px]:flex-row pr-4 sm:pl-14 shadow-2xl mx-1 py-4">
-            <article article className="w-full min-[1250px]:w-3/5 h-65" >
+            {monthly_expense_data.length > 0 ? < article article className="w-full min-[1250px]:w-3/5 h-65" >
                 <h2 className="text-gray-700 font-semibold mb-2 text-center">
-                    Monthly Expenses
+                    Monthly Expenses(Jan - Dec {monthly_expense_data.length != 0 ? monthly_expense_data[0].year : ""})
                 </h2>
                 < ResponsiveContainer width="100 %" height="100%">
-                    <LineChart data={monthly_enpense} >
+                    <LineChart data={monthly_expense_data} >
                         <CartesianGrid strokeDasharray="3 3" />
                         <XAxis dataKey="month" />
                         <YAxis />
@@ -20,13 +21,19 @@ const Charts = ({ expense_category }) => {
                             dot={{ r: 4 }} />
                     </LineChart>
                 </ResponsiveContainer>
-            </article >
+            </article > :
+                <article className="w-full min-[1250px]:w-3/5 h-65 flex flex-col items-center justify-center">
+                    <FaChartLine className="text-5xl mb-3 block text-gray-700" />
+                    <h1 className="text-[1.1rem] font-medium text-gray-900">No expense trend available</h1>
+                    <h1 className="text-sm text-gray-600 font-medium">Add transactions to see monthly spending</h1>
+                </article>
+            }
 
-            <article className="flex items-center flex-col h-79 w-full  min-[1250px]:w-2/5 pt-8">
+            {expense_category.length > 0 ? <article className="flex items-center flex-col h-79 w-full  min-[1250px]:w-2/5 pt-8">
                 <div className="">
                     <h2 className="text-gray-700 font-semibold  text-center">
                         Expense Breakdown
-                        (Jan – Dec 2026)
+                        (Jan - Dec {monthly_expense_data.length != 0 ? monthly_expense_data[0].year : ""})
                     </h2>
                 </div>
                 <ResponsiveContainer width="100%" height="100%">
@@ -62,7 +69,13 @@ const Charts = ({ expense_category }) => {
                         <Tooltip />
                     </PieChart>
                 </ResponsiveContainer>
-            </article>
+            </article> :
+                <article className="flex items-center flex-col h-79 w-full  min-[1250px]:w-2/5  justify-center">
+                    <FaChartPie className="text-5xl mb-3 text-gray-700" />
+                    <p className="text-lg font-medium text-gray-900">No category data</p>
+                    <p className="text-sm font-medium text-gray-500">Add expenses to see breakdown</p>
+                </article>
+            }
         </section >
     )
 }
