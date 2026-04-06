a3734549f239f1ba01070bf12919a5703f34375d
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-04-06 10:10:25 +0530

    feat: implement top spending by month and top spending category logic

--- src/Pages/DashBoard.jsx
+++ src/Pages/DashBoard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 import Navbar from "../Components/navbar"
 import SideBar from "../Components/sidebar"
@@ -6,29 +6,92 @@ import Cards from "../Components/Cards";
 import Charts from "../Components/charts";
 import Transactions from "../Components/TranSactions";
 import { transactionsData } from "../Data/tranSactionsData";
+import Insights from "../Components/insights";
+
 const DashBoard = () => {
     const [role, setRole] = useState("user");
     const [activeTab, setActiveTab] = useState("dashboard");
     const [isSidebarOpen, setIsSidebarOpen] = useState(false)
     const [transactions, setTransactions] = useState(transactionsData);
 
-    const total_income = transactions.filter((transaction) => transaction.type == "income").reduce((sum, transaction) => sum + transaction.amount, 0);
 
-    const total_expense = transactions.filter((transaction) => transaction.type == "expense").reduce((sum, transaction) => sum + transaction.amount, 0);
-    const totalFoodExpense = transactions.filter((transaction) => transaction.category == "Food").reduce((sum, transaction) => sum + transaction.amount, 0);
-    const totalShopingExpense = transactions.filter((transaction) => transaction.category == "Shopping").reduce((sum, transaction) => sum + transaction.amount, 0);
-    const totalTravelExpense = transactions.filter((transaction) => transaction.category == "Travel").reduce((sum, transaction) => sum + transaction.amount, 0);
+    const { total_income, total_expense, expense_category, monthly_expense_data, max_Category, month_having_max_expense } = useMemo(() => {
+        if (transactions.length == 0) {
+            return {
+                total_income: 0,
+                total_expense: 0,
+                expense_category: [],
+                monthly_expense_data: []
+            }
+        }
+        const total_income = transactions.filter((transaction) => transaction.type == "income").reduce((sum, transaction) => sum + transaction.amount, 0);
+        const total_expense = transactions.filter((transaction) => transaction.type == "expense").reduce((sum, transaction) => sum + transaction.amount, 0);
+        const totalFoodExpense = transactions.filter((transaction) => transaction.category == "Food").reduce((sum, transaction) => sum + transaction.amount, 0);
 
-    const totalMedicalExpense = transactions.filter((transaction) => transaction.category == "Medical").reduce((sum, transaction) => sum + transaction.amount, 0);
+        const totalShopingExpense = transactions.filter((transaction) => transaction.category == "Shopping").reduce((sum, transaction) => sum + transaction.amount, 0);
+        const totalTravelExpense = transactions.filter((transaction) => transaction.category == "Travel").reduce((sum, transaction) => sum + transaction.amount, 0);
 
-    const totalEntertainmentExpense = transactions.filter((transaction) => transaction.category == "Entertainment").reduce((sum, transaction) => sum + transaction.amount, 0);
-    const expense_category = [
-        { name: "Food", value: totalFoodExpense },
-        { name: "Shopping", value: totalShopingExpense },
-        { name: "Travel", value: totalTravelExpense },
-        { name: "Medical", value: totalMedicalExpense },
-        { name: "Entertainment", value: totalEntertainmentExpense }
-    ];
+        const totalMedicalExpense = transactions.filter((transaction) => transaction.category == "Medical").reduce((sum, transaction) => sum + transaction.amount, 0);
+
+        const totalEntertainmentExpense = transactions.filter((transaction) => transaction.category == "Entertainment").reduce((sum, transaction) => sum + transaction.amount, 0);
+
+        const expense_category = [
+            { name: "Food", value: totalFoodExpense },
+            { name: "Shopping", value: totalShopingExpense },
+            { name: "Travel", value: totalTravelExpense },
+            { name: "Medical", value: totalMedicalExpense },
+            { name: "Entertainment", value: totalEntertainmentExpense }
+        ];
+        const max_Category = expense_category.reduce((max, category) => category.value > max.value ? category : max);
+
+        const result = {};
+
+        transactions.forEach((tx) => {
+
+            if (tx.type !== "expense") return;
+
+
+            const date = new Date(tx.date);
+
+            const month = date.toLocaleString("en-US", { month: "short", });
+
+            const year = date.toLocaleString("en-US", { year: "numeric" })
+
+            const key = `${month}-${year}`
+            if (!result[key]) {
+                result[key] = {
+                    month,
+                    year,
+                    expense: 0
+                }
+            }
+            result[key].expense += tx.amount
+
+        })
+        const monthly_expense_data = Object.values(result);
+
+        const month_having_max_expense = monthly_expense_data.reduce((max, month) => month.expense > max.expense ? month : max);
+
+        return {
+            month_having_max_expense,
+            total_income,
+            total_expense,
+            expense_category,
+            monthly_expense_data,
+            max_Category
+        }
+    }, [transactions])
+
+
+
+
+
+
+
+
+
+
+
 
     return (
         <div className="h-screen flex flex-col ">
@@ -39,8 +102,9 @@ const DashBoard = () => {
                 </div>
                 {activeTab == "dashboard" && <>
                     <div className="flex flex-col w-full">
-                        <Cards total_income={total_income} total_expense={total_expense} />
-                        <Charts expense_category={expense_category} />
+                        <Cards total_income={total_income} total_expense={total_expense} monthly_expense_data={monthly_expense_data} />
+                        <Charts expense_category={expense_category} monthly_expense_data={monthly_expense_data} />
+                        <Insights max_Category={max_Category} total_expense={total_expense} month_having_max_expense={month_having_max_expense} />
                     </div>
                 </>
                 }
