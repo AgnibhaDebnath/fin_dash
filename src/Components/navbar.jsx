edd7bda52db307d9388d74c1a93255617ecc1db8
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-04-06 17:23:26 +0530

    fix : improve search box UI

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -5,14 +5,14 @@ import { FaUser, FaUserShield } from "react-icons/fa";
 const Navbar = ({ role, setRole, activeTab, setIsSidebarOpen }) => {
 
     return (
-        <nav className="px-4 md:px-5 py-6 flex flex-row justify-between bg-white shadow-xl fixed top-0 w-full z-50">
-            {activeTab == "dashboard" && <h1 className="font-bold text-3xl pb-1">
+        <nav className="px-4 md:px-5 py-5.5 flex flex-row justify-between bg-white shadow-xl fixed top-0 w-full z-50 items-center ">
+            {activeTab == "dashboard" && <h1 className="font-bold text-4xl pb-1">
                 <button onClick={() => setIsSidebarOpen(true)} className="cursor-pointer min-[768px]:hidden">
                     <LuAlignJustify size={25} className="mt-3" />
                 </button>
                 <span className="ml-5">Dashboard</span>
             </h1>}
-            {activeTab == "transactions" && <h1 className="font-bold text-3xl pb-1">
+            {activeTab == "transactions" && <h1 className="font-bold text-4xl pb-1">
                 <button onClick={() => setIsSidebarOpen(true)} className="cursor-pointer min-[768px]:hidden">
                     <LuAlignJustify size={25} className="mt-3" />
                 </button>
@@ -20,7 +20,7 @@ const Navbar = ({ role, setRole, activeTab, setIsSidebarOpen }) => {
             </h1>}
             <div className="relative hidden md:block">
                 <FaSearch className="absolute top-2.5 left-3" />
-                <input type="text" placeholder="Search..." className="border-2 rounded-full pl-8 pr-2 py-1 text-justify hidden md:block " />
+                <input type="text" placeholder="Search..." className="border-2 border-gray-400 rounded-full pl-8 pr-2 py-1 text-justify hidden md:block shadow-sm focus:outline-none focus:ring-2 focus:border-blue-500  focus:ring-blue-500" />
             </div>
             <div className=" hidden md:block ">
                 <button
