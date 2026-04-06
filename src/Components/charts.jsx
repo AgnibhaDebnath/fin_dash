f45a27a36b2b6ad305857cde0a5f25e640cdb234
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-04-06 16:33:59 +0530

    fix : add margin to chart heading

--- src/Components/charts.jsx
+++ src/Components/charts.jsx
@@ -8,7 +8,7 @@ const Charts = ({ expense_category, monthly_expense_data }) => {
     return (
         <section className="mt-6 flex flex-col items-center justify-evenly min-[1250px]:flex-row pr-4 sm:pl-14 shadow-2xl mx-1 py-4 gap-10  min-[1250px]:gap-0">
             {monthly_expense_data.length > 0 ? < article className="w-full min-[1250px]:w-3/5 h-65" >
-                <h2 className="text-gray-700 font-semibold mb-2 text-center">
+                <h2 className="text-gray-700 font-semibold mb-6 text-center">
                     Monthly Expenses(Jan - Dec {monthly_expense_data.length != 0 ? monthly_expense_data[0].year : ""})
                 </h2>
                 < ResponsiveContainer width="100 %" height="100%">
