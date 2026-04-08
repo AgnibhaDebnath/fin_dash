2a61d76b6c0490211f02ba8968d4eb22fa14629f
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-04-08 15:45:50 +0530

    fix: improve UI and fix footer position for mobile

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
-        <footer className="text-center text-gray-600 text-sm font-medium py-3 mt-6">
-            © 2026 FinDash
+        <footer className="text-center text-gray-600 text-sm font-medium py-3 mt-6 h-full flex items-end justify-center">
+            <h1 className="h-5">© 2026 FinDash</h1>
         </footer>
     )
 }
