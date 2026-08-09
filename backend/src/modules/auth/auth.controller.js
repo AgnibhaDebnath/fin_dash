5650050567d987e44be1a5d3a5892535d2dc0b89
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-08-09 08:52:56 +0530

    feat: implement login, logout, and getCurrentUser controllers

--- backend/src/modules/auth/auth.controller.js
+++ backend/src/modules/auth/auth.controller.js
@@ -1,15 +1,16 @@
 import { success } from "zod";
-import signupService from "./auth.service.js";
+import { signupService, loginService,getCurrentUserData } from "./auth.service.js";
 import dotenv from "dotenv"
 
 dotenv.config()
 
 const signupController =async (req, res, next) => {
+    try {
+        
     
     const user = await signupService(req.body);
     if (!user.success) {
        return res.status(409).json({
-            success: false,
             message: "An account with this email already exists."
         })
     }
@@ -20,8 +21,79 @@ const signupController =async (req, res, next) => {
         maxAge:24 * 60 * 60 * 1000
     })
     res.status(201).json({
-        success: true,
         message: "Account created successfully.",
-    }) 
+    })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: "Couldn't create acount. Please try again later."
+        })
+    }    
 }
-export default signupController;
+
+
+const loginController = async (req, res, next) => {
+    try {
+        
+    
+    const result = await loginService(req.body);
+    if (!result.success) {
+        return res.status(401).json({
+            success: false,
+            message:result.message
+      })
+    }
+   
+    res.cookie("token", result.token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge:24 * 60 * 60 * 1000
+    })
+    res.status(200).json({
+        success: true,
+        message: "Login successful"
+    }) 
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: "Couldn't log in. Please try again later."
+        })
+        }
+}
+
+const getCurrentUser = async (req, res, next) => {
+    try {
+        
+        const user = await getCurrentUserData(req.user.id);
+        
+        res.status(201).json({
+            success: true,
+            user
+        })
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const logoutController = (req, res) => {
+    try {
+        
+   
+    res.clearCookie("token", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge:24 * 60 * 60 * 1000
+    })
+
+    res.status(200).json({
+        success: true,
+        message:"Logged out successfully"
+    })
+    } catch (err) {
+        console.log(err)
+    }
+}
+ 
+export { signupController, loginController,getCurrentUser,logoutController };
