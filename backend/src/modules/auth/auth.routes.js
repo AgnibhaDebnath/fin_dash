a7e49626d04153f1207fdf5ac3f069c9aa997b96
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-08-09 08:55:48 +0530

    feat: add login, me, and logout routes

--- backend/src/modules/auth/auth.routes.js
+++ backend/src/modules/auth/auth.routes.js
@@ -1,9 +1,13 @@
 import express from "express"
-import signupController from "./auth.controller.js";
-import signupValidator from "./auth.validator.js";
+import { signupController ,loginController,getCurrentUser} from "./auth.controller.js";
+import { signupValidator,loginValidator} from "./auth.validator.js";
+import authenticate from "../../middlewares/auth.middleware.js";
+import { logoutController } from "./auth.controller.js";
 const authRoutes = express.Router();
 
 
-authRoutes.post("/auth/signup",signupValidator,signupController);
-
+authRoutes.post("/signup",signupValidator,signupController);
+authRoutes.post("/login", loginValidator, loginController);
+authRoutes.get("/me", authenticate, getCurrentUser);
+authRoutes.post("/logout",logoutController)
 export default authRoutes;
