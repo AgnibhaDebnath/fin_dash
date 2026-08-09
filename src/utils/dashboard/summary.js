c361ded331393f71299438da8329c30eded018e0
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-08-09 08:28:18 +0530

    feat: add utility function to calculate income, expense, and balance

---
+++ src/utils/dashboard/summary.js
@@ -0,0 +1,20 @@
+export const summary = (transactions) => {
+    let income = 0;
+    let expense = 0;
+    transactions?.forEach(transaction => {
+        if (transaction.type === "income") {
+            income += Number(transaction.amount);  
+        }
+        else{
+            expense += Number(transaction.amount);
+        
+        }
+    });
+    
+    return {
+        income,
+        expense,
+        balance:income - expense,
+    }
+
+}
