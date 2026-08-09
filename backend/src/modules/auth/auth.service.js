b3070ef531a597168998f4301232190a1fe9e5ac
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-08-09 08:57:37 +0530

    feat: implement login and getCurrentUser services

--- backend/src/modules/auth/auth.service.js
+++ backend/src/modules/auth/auth.service.js
@@ -1,6 +1,7 @@
 import User from "./user.model.js"
 import hashPassowrd from "../../utils/hashPassword.js"
-import generateToken from "../../utils/generateToken.js"
+import { generateToken } from "../../utils/jwt.js"
+import comparePassword from "../../utils/comparePassword.js"
 
 const signupService = async (userData) => {
     const user = await User.findOne({ email: userData.email });
@@ -25,4 +26,28 @@ const signupService = async (userData) => {
 
 
 }
-export default signupService;
+
+const loginService = async (userData) => {
+    
+    const user = await User.findOne({ email: userData.email });
+    
+    if (!user) {
+        return { success: false,message:"Invalid email or password." } 
+    }
+    const isMatch = await comparePassword(userData.password, user.password);
+   
+    if (!isMatch) {
+        return {success:false,message:"Invalid email or password."}
+    }
+    const token = generateToken(user);
+        return {
+        success:true,
+        token
+    }
+}
+
+const getCurrentUserData =async (id) => {
+    return await User.findById(id).select("-password -__v -createdAt -updatedAt")
+}
+
+export { signupService, loginService,getCurrentUserData };
