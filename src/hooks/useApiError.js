806c965cdcad200bf44242c9fa93bbfbd6a347df
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-08-09 09:12:21 +0530

    feat: add useApiError hook for centralized error handling

---
+++ src/hooks/useApiError.js
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router";
+import { toast } from "react-toastify";
+import { AuthContext } from "@/context/AuthContext";
+import { useContext } from "react";
+export function useApiError() {
+    const navigate = useNavigate();
+const { logout}=useContext(AuthContext)
+    return (err) => {
+        if (err instanceof TypeError) {
+            toast.error("Unable to connect. Please check your internet connection.");
+            return;
+        }
+
+        if (err.status === 401) {
+            toast.error(err.message)
+            logout()
+            navigate("/login", { replace: true });
+            return;
+    }
+    
+  toast.error(err.message);
+  }  
+}
