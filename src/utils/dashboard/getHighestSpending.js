037ba7afce448cdb5d423a1c28652e948405c726
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-08-12 23:33:26 +0530

    feat: add utility to get highest spending by date filter

---
+++ src/utils/dashboard/getHighestSpending.js
@@ -0,0 +1,16 @@
+import { getHighestSpendingDay } from "./getHighestSpendingDay";
+import { getHighestSpendingMonth } from "./getHighestSpendingMonth";
+export const getHighestSpending = (transactions, dateFilter) => {
+    let data = [];
+    switch (dateFilter) {
+        case "this-month":
+        case "last-month":
+        case "this-week":
+            return (data = getHighestSpendingDay(transactions));
+        case "this-year":
+        case "all-time":
+            return (data = getHighestSpendingMonth(transactions));
+        default:
+            return data;
+    }
+};
