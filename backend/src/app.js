59a4cb48add690e42cb9eb87f4ea16a243037491
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-08-23 19:21:39 +0530

    feat: register transaction routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,21 @@
-import express from "express"
+import express from "express";
 import authRoutes from "./modules/auth/auth.routes.js";
-import cors from "cors"
+import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import transactionRoutes from "./modules/transaction/transaction.route.js";
 
 const app = express();
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-}));
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    }),
+);
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api",authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/transaction", transactionRoutes);
 export default app;
