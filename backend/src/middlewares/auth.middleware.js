2dde0390ccc116c21111016a5efb59d02dc3cb1c
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-08-23 20:18:16 +0530

    chore: fix lint error and format auth middleware

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,28 +1,26 @@
-import { verifyToken } from "../utils/jwt.js"
+import { verifyToken } from "../utils/jwt.js";
 const authenticate = async (req, res, next) => {
     try {
-        
-   
-    const token = req.cookies.token;
+        const token = req.cookies.token;
 
         if (!token) {
-      return res.status(401).json({
-        success: false,
-        message: "Authentication required",
-      });
-    }
+            return res.status(401).json({
+                success: false,
+                message: "Authentication required",
+            });
+        }
         const decoded = await verifyToken(token);
-        
+
         req.user = {
             id: decoded.userID,
         };
-
     } catch (err) {
+        console.log(err);
         return res.status(401).json({
             success: false,
             message: "Token expired or invalid",
         });
     }
     next();
-}
-export default authenticate
+};
+export default authenticate;
