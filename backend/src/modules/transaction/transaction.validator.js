1089df62ce4f3969eba0592cab9793b68ed0d185
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-08-23 19:49:16 +0530

    refactor: use null instead of undefined for empty amount

--- backend/src/modules/transaction/transaction.validator.js
+++ backend/src/modules/transaction/transaction.validator.js
@@ -1,46 +1,59 @@
 import { z } from "zod";
 const TransactionSchema = z.object({
-        title: z
-            .string()
-            .trim()
+    title: z
+        .string()
+        .trim()
         .min(1, "Title is required")
         .max(40, "Title must be less than 30 characters"),
 
-        amount: z.preprocess(value => value == "" ? undefined : Number(value),
-            z.number({ error: "Amount is required" })
-                .positive("Amount must be greater than 0"),
-        ),
-        type: z.enum(["income", "expense"], {
-            error: "Please select a transaction type",
-        }),
+    amount: z.preprocess(
+        (value) => (value === "" ? null : Number(value)),
+        z.number({ error: "Amount is required" }).positive("Amount must be greater than 0"),
+    ),
+    type: z.enum(["income", "expense"], {
+        error: "Please select a transaction type",
+    }),
 
-        category: z.enum(["food", "transport", "Shopping", "bills", "salary", "healthcare", "entertainment", "household", "education", "investment","freelance"], {
-            error: "Please select a category"
-        }),
-    date:z.coerce.date({
-            error: "Please select a date",
-        }),
-    
-    });
+    category: z.enum(
+        [
+            "food",
+            "transport",
+            "shopping",
+            "bills",
+            "salary",
+            "healthcare",
+            "entertainment",
+            "household",
+            "education",
+            "investment",
+            "freelance",
+        ],
+        {
+            error: "Please select a category",
+        },
+    ),
+    date: z.coerce.date({
+        error: "Please select a date",
+    }),
+});
 
 const transactionFormValidator = (req, res, next) => {
-
-    const result = TransactionSchema.safeParse(req.body)
+    const result = TransactionSchema.safeParse(req.body);
 
     if (!result.success) {
-         const tree = z.treeifyError(result.error);
-            
-       return res.status(400).json({
-            errors:{
+        const tree = z.treeifyError(result.error);
+
+        return res.status(400).json({
+            errors: {
                 title: tree.properties?.title?.errors ?? [],
                 amount: tree.properties?.amount?.errors ?? [],
                 type: tree.properties?.type?.errors ?? [],
                 category: tree.properties?.category?.errors ?? [],
                 date: tree.properties?.date?.errors ?? [],
-            }
-        })
+            },
+        });
     }
-    next()
-}
+    next();
+};
 
 export default transactionFormValidator;
