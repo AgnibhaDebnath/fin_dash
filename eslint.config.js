cf4e59e65db8555cca78b1f74ae078716592b00b
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-08-28 01:45:13 +0530

    chore: customize ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,41 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import reactHooks from "eslint-plugin-react-hooks";
+import prettier from "eslint-config-prettier";
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+    {
+        ignores: ["node_modules", "dist", "backend"],
     },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    {
+        files: ["**/*.{js,jsx}"],
+        plugins: {
+            js,
+            "react-hooks": reactHooks,
+        },
+        extends: ["js/recommended"],
+        languageOptions: { globals: globals.browser },
+
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+        },
     },
-  },
-])
+    pluginReact.configs.flat.recommended,
+    {
+        files: ["**/*.{js,jsx}"],
+        rules: {
+            "react/react-in-jsx-scope": "off",
+            "react/prop-types": "off",
+        },
+    },
+
+    prettier,
+]);
