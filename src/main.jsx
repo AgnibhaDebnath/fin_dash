98b33221119c0858c7ffcee3a1c58a3570f0509f
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-08-28 01:57:01 +0530

    feat: add BrowserRouter, AuthProvider, and FilterProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from "react-router"
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+import React from "react";
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import { BrowserRouter } from "react-router";
+import AuthProvider from "./context/AuthContext.jsx";
+import FilterProvider from "./context/FilterContext";
+import App from "./App.jsx";
+import "./index.css";
+import "./App.css";
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <AuthProvider>
+            <FilterProvider>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </FilterProvider>
+        </AuthProvider>
+    </StrictMode>,
+);
