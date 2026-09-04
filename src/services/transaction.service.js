6d75ed062b55136a38ade89fde1a50764e4543e5
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-09-04 10:56:28 +0530

    feat: return total transactions across all time

--- src/services/transaction.service.js
+++ src/services/transaction.service.js
@@ -1,26 +1,21 @@
 export const fetchTransactions = async (params = "") => {
-          const url = params
-    ? `http://localhost:3001/api/transaction?${params}`
+    const url = params
+        ? `http://localhost:3001/api/transaction?${params}`
         : `http://localhost:3001/api/transaction`;
-    
-        const res = await fetch(url,
-            {
-                credentials: "include",
-                method: "GET"
-            },
 
-        )
-        const data = await res.json();
-        
+    const res = await fetch(url, {
+        credentials: "include",
+        method: "GET",
+    });
+    const data = await res.json();
+
     if (!res.ok) {
-        console.log(data)
+        console.log(data);
         const error = new Error(data.message);
         error.status = res.status;
-        throw error
-       
+        throw error;
     }
 
-        const { totalTransactions, transactions } = data;
-        return { totalTransactions, transactions };
- 
-    }
+    const { totalTransactions, transactions, totalTransactionsAllOverTime } = data;
+    return { totalTransactions, transactions, totalTransactionsAllOverTime };
+};
