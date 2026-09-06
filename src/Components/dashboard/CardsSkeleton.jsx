b1929fa51b87d7e6af97c1dde931eb75da902105
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-09-07 02:24:37 +0530

    style: adjust dashboard card skeleton widths

--- src/Components/dashboard/CardsSkeleton.jsx
+++ src/Components/dashboard/CardsSkeleton.jsx
@@ -1,10 +1,9 @@
-
 const CardsSkeleton = () => {
     return (
-        <div className="flex w-full flex-col gap-y-2 h-10 justify-center animate-pulse">
-            <div className="h-3 w-1/2 bg-gray-200 rounded-3xl"></div>
-            <div className="h-3 w-1/4 bg-gray-200 rounded-3xl"></div>
+        <div className="flex flex-col gap-y-2 h-10 justify-center animate-pulse">
+            <div className="h-3 w-30 bg-gray-200 rounded-3xl"></div>
+            <div className="h-3 w-15 bg-gray-200 rounded-3xl"></div>
         </div>
-    )
-}
+    );
+};
 export default CardsSkeleton;
