768d02226fecf7fe0323ad9eede9962f50774b59
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-09-07 02:08:55 +0530

    refactor: simplify sidebar navigation with NavLink

--- src/Components/layout/Sidebar.jsx
+++ src/Components/layout/Sidebar.jsx
@@ -1,80 +1,101 @@
-import { FaHouse } from "react-icons/fa6";
-import { FaList } from "react-icons/fa";
-import { useState } from "react";
-import { FaChevronDown, FaSearch, FaUser, FaUserShield, FaEye } from "react-icons/fa";
-
-
-const SideBar = ({ setActiveTab, activeTab, role, setRole }) => {
-    const [isOpen, setIsOpen] = useState(false)
+import { NavLink } from "react-router";
+import Logo from "../../assets/logo.svg";
+import { AuthContext } from "@/context/AuthContext";
+import { useContext } from "react";
+import { FaUser } from "react-icons/fa";
+import { PiSquaresFourFill } from "react-icons/pi";
+import { FaExchangeAlt } from "react-icons/fa";
+import { FaGear } from "react-icons/fa6";
+import { MdLogout } from "react-icons/md";
+const SideBar = () => {
+    const { user, logout } = useContext(AuthContext);
     return (
-
-        <aside className="w-60 left-0 fixed border-t-2  bg-white shadow-2xl h-[calc(100vh-4rem)] flex flex-col  border-gray-400 p-4 top-23 min-[768px]:top-22">
-            <div className="flex items-center gap-2 py-4 ">
-                <div className="w-10 h-10 bg-blue-500 text-white flex items-center justify-center rounded-full font-bold">
-                    <h1 className="font-bold text-2xl font-serif">F</h1>
-                </div>
-                <span className="text-3xl font-bold">FinDash</span>
-            </div>
-
-            <div className="md:hidden relative">
-                <input type="text" placeholder="Search..." className="border-2 w-full box-border rounded-full pl-8 py-0.5 pb-1 text-justify shadow-sm focus:outline-none focus:ring-2 focus:border-blue-500  focus:ring-blue-500 border-gray-400" />
-                <FaSearch className="absolute top-2.5 left-3" />
-            </div>
-
-
-            <div className={`py-3 flex  items-center ml-2 `}>
-                <FaHouse className="inline mr-2" />
-                <button onClick={() => setActiveTab("dashboard")} className={`font-medium  hover:scale-105 transition duration-150 cursor-pointer px-4 rounded-lg py-0.5 ${activeTab == "dashboard" ? "text-blue-600  bg-blue-100" : "text-gray-700 hover:bg-gray-100  "}`}>Dashboard</button>
-            </div>
-            <div className="py-3 flex  items-center ml-2">
-                <FaList className="inline mr-2" />
-                <button onClick={() => setActiveTab("transactions")} className={`font-medium  hover:scale-105 transition duration-150 cursor-pointer px-4 rounded-lg py-0.5 ${activeTab == "transactions" ? "text-blue-500 bg-blue-100" : "text-gray-700 hover:bg-gray-100"}`}>Transactions</button>
-            </div>
-            <div className="relative md:hidden flex items-center ml-2">
-
-                <FaEye className="inline" />
-                <button
-                    onClick={() => setIsOpen(!isOpen)}
-                    className="flex items-center gap-1 px-3 py-2 font-medium cursor-pointer"
-                >
-
-                    Viewer
-                    <FaChevronDown
-                        className={`transition-transform duration-200 mt-1 ${isOpen ? "rotate-180" : ""
-                            }`}
-                    />
-                </button>
-
-
-                {isOpen && (
-                    <div className="absolute left-10 w-32 z-50 top-10 flex  flex-col items-start">
-
-                        <button
-                            onClick={() => {
-                                setRole("user");
-                                setIsOpen(false);
-                            }}
-                            className={`block hover:scale-105 cursor-pointer transition duration-150 ${role == "user" ? "text-blue-500" : "text-gray-500"}`}
-                        >
-                            <FaUser className="inline mb-2" /> User
-                        </button>
-
-                        <button
-                            onClick={() => {
-                                setRole("admin");
-                                setIsOpen(false);
-                            }}
-                            className={`block hover:scale-105 cursor-pointer transition duration-150 ${role == "admin" ? "text-blue-500" : "text-gray-500"}`}
-                        >
-                            < FaUserShield className="inline mb-1" /> Admin
-                        </button>
-
+        <aside className="w-60 left-0 fixed bg-white shadow-2xl h-[calc(100vh-4rem)] flex flex-col p-4 top-22 min-[768px]:top-20 font-[inter] justify-between pb-10">
+            <div>
+                <div className="flex items-center gap-2 py-4 ">
+                    <div>
+                        <img src={Logo} alt="Application Logo" className="w-12 h-12" />
                     </div>
-                )}
+                    <h2 className="text-2xl font-bold">
+                        Expense<span className="text-indigo-600">Flow</span>
+                    </h2>
+                </div>
+                <hr />
+
+                <NavLink
+                    to="/dashboard"
+                    className={({ isActive }) =>
+                        `py-3.5 flex items-center px-3 mt-2 rounded-md transition duration-300 hover:scale-105 ${
+                            isActive
+                                ? "bg-indigo-50 text-indigo-600"
+                                : "hover:bg-gray-100 text-gray-600"
+                        }`
+                    }
+                >
+                    {({ isActive }) => (
+                        <>
+                            <PiSquaresFourFill
+                                className={`mr-2 ${isActive ? "text-indigo-600" : "text-gray-600"}`}
+                                size={21}
+                            />
+                            <span
+                                className={`font-medium tracking-wide text-[1.1rem]  ${
+                                    isActive ? "text-indigo-600" : "text-gray-500"
+                                }`}
+                            >
+                                Dashboard
+                            </span>
+                        </>
+                    )}
+                </NavLink>
+
+                <NavLink
+                    to="/transactions"
+                    className={({ isActive }) =>
+                        `py-3.5 flex items-center px-3 mt-2 rounded-md
+                transition duration-300 hover:scale-105 ${
+                    isActive ? "bg-indigo-50 text-indigo-600" : "hover:bg-gray-100 text-gray-600"
+                }`
+                    }
+                >
+                    {({ isActive }) => (
+                        <>
+                            <FaExchangeAlt
+                                className={`mr-2 ${isActive ? "text-indigo-600" : "text-gray-600"}`}
+                                size={16}
+                            />
+                            <span
+                                className={`font-medium tracking-wide text-[1.1rem] ${
+                                    isActive ? "text-indigo-600" : "text-gray-500"
+                                }`}
+                            >
+                                Transactions
+                            </span>
+                        </>
+                    )}
+                </NavLink>
             </div>
-
+            <div className="min-[768px]:hidden">
+                <div className="flex flex-col pl-4 ">
+                    <div className="flex gap-x-2 items-center">
+                        <FaUser />
+                        <span className="font-semibold">{user.fullName}</span>
+                    </div>
+                    <span className="font-medium text-sm ml-6">{user.email}</span>
+                </div>
+                <hr className="mt-2" />
+                <button className=" w-full px-4 py-2 text-left hover:bg-gray-100 cursor-pointer flex items-center font-medium rounded-md mt-2">
+                    <FaGear className="mr-2" /> Settings
+                </button>
+                <button
+                    className=" w-full px-4 py-2 text-left hover:bg-red-100 cursor-pointer flex items-center font-medium rounded-md "
+                    onClick={() => logout()}
+                >
+                    <MdLogout size={18} className="mr-2 text-red-500 " /> Logout
+                </button>
+            </div>
         </aside>
-    )
-}
+    );
+};
 
-export default SideBar
+export default SideBar;
