fb59a6232701e6be3b83ff30d828464a0dd5cc26
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-09-07 01:49:19 +0530

    style: update font styles and format code

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router";
 const NotFound = () => {
     return (
-        <div className='flex justify-center items-center w-full h-screen'>
-            <div className='flex flex-col items-center gap-8'>
+        <div className="flex justify-center items-center w-full h-screen font-[inter]">
+            <div className="flex flex-col items-center gap-8">
                 <div>
-                    <h1 className='text-8xl min-[450px]:text-9xl font-bold text-gray-700 '>404</h1>
+                    <h1 className="text-8xl min-[450px]:text-9xl font-bold text-gray-700 ">404</h1>
                 </div>
                 <div>
-                    <h1 className='text-4xl min-[450px]:text-5xl font-bold text-gray-700'>Page not found</h1>
+                    <h1 className="text-4xl min-[450px]:text-5xl font-bold text-gray-700">
+                        Page not found
+                    </h1>
                 </div>
                 <Link
                     to="/"
@@ -17,9 +19,8 @@ const NotFound = () => {
                     Back to Home
                 </Link>
             </div>
-
         </div>
-    )
-}
+    );
+};
 
-export default NotFound
+export default NotFound;
