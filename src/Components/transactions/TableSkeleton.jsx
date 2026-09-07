b3bdfb2cee75e395d567dc612195d8a16b10ab66
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-09-07 12:39:12 +0530

    refactor: align table skeleton with transaction table

--- src/Components/transactions/TableSkeleton.jsx
+++ src/Components/transactions/TableSkeleton.jsx
@@ -1,54 +1,59 @@
-import React from 'react'
+import React from "react";
 
 const TableSkeleton = () => {
     return (
-        <div className='overflow-x-auto hide-scrollbar'>
-            <div className='w-full animate-pulse flex justify-between h-40 px-10 min-w-240'>
-                <div className='flex flex-col w-14/100 justify-between'>
-                    <div className='w-full h-8 bg-gray-200 rounded-lg' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
-                </div>
-                <div className='flex flex-col w-16/100 justify-between'>
-                    <div className='w-full h-8 bg-gray-200 rounded-lg' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
-                </div>
-                <div className='flex flex-col w-16/100 justify-between'>
-                    <div className='w-full h-8 bg-gray-200 rounded-lg' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
-                </div>
-                <div className='flex flex-col w-12/100 justify-between'>
-                    <div className='w-full h-8 bg-gray-200 rounded-lg' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
-                </div>
-                <div className='flex flex-col w-12/100 justify-between'>
-                    <div className='w-full h-8 bg-gray-200 rounded-lg' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
-                </div>
-                <div className='flex flex-col w-16/100 justify-between'>
-                    <div className='w-full h-8 bg-gray-200 rounded-lg' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
-                    <div className='w-full h-3 bg-gray-200 rounded-3xl' />
+        <div className="w-full px-0 min-[400px]:px-2 sm:px-5 lg:px-10">
+            <div className="overflow-x-auto hide-scrollbar rounded-xl border p-5 shadow-md">
+                <div className="w-full animate-pulse h-45 min-w-240">
+                    <div className="h-12 bg-gray-100 mb-3"></div>
+                    <div className="w-full flex justify-between">
+                        <div className="flex flex-col h-28 w-13/100 justify-between">
+                            <div className="w-full h-3 bg-gray-200 rounded-lg" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                        </div>
+                        <div className="flex flex-col w-19/100 justify-between">
+                            <div className="w-full h-3 bg-gray-200 rounded-lg" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                        </div>
+                        <div className="flex flex-col w-15/100 justify-between">
+                            <div className="w-full h-3 bg-gray-200 rounded-lg" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                        </div>
+                        <div className="flex flex-col w-12/100 justify-between">
+                            <div className="w-full h-3 bg-gray-200 rounded-lg" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                        </div>
+                        <div className="flex flex-col w-12/100 justify-between">
+                            <div className="w-full h-3 bg-gray-200 rounded-lg" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                        </div>
+                        <div className="flex flex-col w-15/100 justify-between">
+                            <div className="w-full h-3 bg-gray-200 rounded-lg" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                            <div className="w-full h-3 bg-gray-200 rounded-3xl" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default TableSkeleton
+export default TableSkeleton;
