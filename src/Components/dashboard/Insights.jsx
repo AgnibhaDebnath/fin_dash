2821fb0ade89adc4c791f989e9e0c2a9b314f374
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-09-07 09:46:13 +0530

    refactor: rebuild insights section with utility calculations

--- src/Components/dashboard/Insights.jsx
+++ src/Components/dashboard/Insights.jsx
@@ -1,40 +1,94 @@
-import { FaChartPie, FaCalendarAlt, FaInbox, FaUtensils, FaShoppingCart, FaPlane, FaHeartbeat, FaFilm } from "react-icons/fa";
+import { FilterContext } from "@/context/FilterContext";
+import Footer from "../layout/Footer";
+import { useContext } from "react";
+import { getHighestSpendingHeading } from "@/utils/dashboard/getHighestSpendingHeading";
+import { getHighestSpending } from "@/utils/dashboard/getHighestSpending";
+import { FaRupeeSign, FaCalendarAlt, FaTrophy } from "react-icons/fa";
 
-import Footer from "./footer";
-const Insights = ({ max_Category, total_expense, month_having_max_expense }) => {
+import { formatSpendingPeriod } from "@/utils/dashboard/formatSpendingPeriod";
+import { getTopSpendingCategory } from "@/utils/dashboard/getTopSpendingCategory";
+import { categories } from "@/constants/transaction/categories";
+import CardsSkeleton from "./CardsSkeleton";
+const Insights = ({ transactions, isLoading }) => {
+    const { dashboardDateFilter } = useContext(FilterContext);
+
+    const { highestSpendingPeriod, highestPeriodAmount } = getHighestSpending(
+        transactions,
+        dashboardDateFilter,
+    );
+    const { highestSpendingCategory, highestCategoryAmount } = getTopSpendingCategory(transactions);
+
+    const categoryInfo = categories.find((category) => category.value === highestSpendingCategory);
+    const Icon = categoryInfo?.icon;
     return (
-        <section className="w-full flex flex-col  justify-center mb-5">
+        <section className="w-full flex flex-col  justify-center mb-5 font-[inter]">
             <artical className="w-full flex justify-center">
-                <h1 className="text-gray-800 text-3xl font-bold my-8">
-                    Summary
-                </h1>
+                <h1 className="text-gray-800 text-3xl font-bold my-8">Summary</h1>
             </artical>
             <div className="flex justify-center flex-col min-[1000px]:flex-row pl-0 min-[490px]:pl-14 gap-4 min-[490px]:pr-3 pr-0">
-                <article className="w-full flex justify-center">
-                    <div className="w-10/11 min-[1000px]:w-full shadow-md hover:shadow-lg rounded-2xl my-2 py-4 flex flex-col justify-center items-center" >
-                        <h1 className="font-bold text-xl text-gray-600"><FaChartPie className="inline mb-0.5 text-3xl mr-2" />Top Category Spending</h1>
-                        {max_Category ? <>
-                            {max_Category.name === "Food" ? <p><FaUtensils className="inline mr-2 text-orange-500 " />{max_Category.name}</p> : max_Category.name === "Shopping" ? <p><FaShoppingCart className="inline mr-2 text-blue-500" />{max_Category.name}</p> : max_Category.name === "Travel" ? <p><FaPlane className="inline text-purple-500 mr-2" />{max_Category.name}</p> : max_Category.name === "Medical" ? <p><FaHeartbeat className="inline text-red-500 mr-2" />{max_Category.name}</p> : max_Category.name === "Entertainment" ? <p><FaFilm className="inline text-pink-500 mr-2" />{max_Category.name}</p> : ""}
-                            <p className="font-extrabold text-[1.2rem] text-red-500 font-[Lato]">₹ {max_Category.value}</p>
-                            <p className="text-gray-600">{((max_Category.value / total_expense) * 100).toFixed(0)}% of total expense</p>
-                            <p className="text-gray-600">Highest spending category</p>
-                        </> : <p>No data available</p>}
+                <div className="w-full flex justify-center">
+                    <div className="w-10/11 min-[1000px]:w-full shadow-md hover:shadow-lg rounded-2xl my-2 py-4 flex flex-col justify-center items-center">
+                        <h4 className="font-bold text-lg text-gray-900">
+                            {" "}
+                            <FaCalendarAlt className="inline text-blue-500 mb-1.5 mr-2" size={23} />
+                            {getHighestSpendingHeading(dashboardDateFilter)}
+                        </h4>
+                        {isLoading ? (
+                            <div className="w-full flex justify-center">
+                                <CardsSkeleton />
+                            </div>
+                        ) : highestPeriodAmount > 0 ? (
+                            <>
+                                <p className="mt-2">
+                                    {formatSpendingPeriod(
+                                        highestSpendingPeriod,
+                                        dashboardDateFilter,
+                                    )}
+                                </p>
+                                <p className="text-rose-500 text-2xl font-semibold mt-0.5">
+                                    <FaRupeeSign size={20} className="inline mb-1" />
+                                    {highestPeriodAmount}
+                                </p>
+                            </>
+                        ) : (
+                            <p className="text-gray-500">No spending</p>
+                        )}
                     </div>
-                </article>
-                <article className="w-full flex justify-center">
-                    <div className="w-10/11 min-[1000px]:w-full shadow-md hover:shadow-lg rounded-2xl my-2 py-4 flex flex-col justify-center items-center" >
-                        <h1 className="font-bold text-xl text-gray-600"><FaCalendarAlt className="inline mr-2 mb-1 text-2xl" />Top Monthly Spending</h1>
-                        {month_having_max_expense ? <>
-                            <p>{month_having_max_expense.month}</p>
-                            <p className="font-extrabold text-[1.2rem] text-red-500 font-[Lato]">₹ {month_having_max_expense.expense}</p>
-                            <p className="text-gray-600">{((month_having_max_expense.expense / total_expense) * 100).toFixed(0)}% of total expense</p>
-                            <p className="text-gray-600">Peak spending month</p>
-                        </> : <p>No data available</p>}
+                </div>
+                <div className="w-full flex justify-center">
+                    <div className="w-10/11 min-[1000px]:w-full shadow-md hover:shadow-lg rounded-2xl my-2 py-4 flex flex-col justify-center items-center">
+                        <h4 className="font-bold text-lg text-gray-900">
+                            <FaTrophy className="inline text-orange-400 mb-1" size={24} /> Top
+                            Spending Category
+                        </h4>
+                        {isLoading ? (
+                            <div className="flex justify-center w-full">
+                                <CardsSkeleton />
+                            </div>
+                        ) : highestSpendingCategory ? (
+                            <>
+                                <p
+                                    className={` font-medium text-[1rem] text-gray-800 tracking-wide mt-2`}
+                                >
+                                    <Icon
+                                        className={`inline mb-1.5 ${categoryInfo?.color}`}
+                                        size={20}
+                                    />{" "}
+                                    {categoryInfo?.label}
+                                </p>
+                                <p className="text-rose-500 text-2xl font-semibold">
+                                    <FaRupeeSign size={20} className="inline mb-1" />
+                                    {highestCategoryAmount}
+                                </p>
+                            </>
+                        ) : (
+                            <p className="text-gray-500">No expenses recorded</p>
+                        )}
                     </div>
-                </article>
+                </div>
             </div>
             <Footer />
         </section>
-    )
-}
+    );
+};
 export default Insights;
