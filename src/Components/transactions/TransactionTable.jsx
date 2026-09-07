0cad845a1e28a3e5a62cd97dc6d8bba49b032e9b
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-09-07 12:12:07 +0530

    feat: add pagination and loading skeleton to transaction table

--- src/Components/transactions/TransactionTable.jsx
+++ src/Components/transactions/TransactionTable.jsx
@@ -2,36 +2,52 @@ import TransactionRow from "./TransactionRow";
 
 import NoResult from "./NoResults";
 import EmptyTransactions from "./EmptyTransactions";
-const TransactionTable = ({ transactions, totalTransactions, setIsFormOpen, setEditingTransaction, setIsDeleteDialogOpen, setSelectedTransaction }) => {
-    const tableHeaders = [
-        "Date",
-        "Title",
-        "Category",
-        "Amount",
-        "Type",
-        "Actions",
-    ];
-
-
+import Pagination from "./Pagination";
+import TableSkeleton from "./TableSkeleton";
+import Footer from "../layout/Footer";
+const TransactionTable = ({
+    transactions,
+    totalTransactions,
+    setIsFormOpen,
+    setEditingTransaction,
+    setIsDeleteDialogOpen,
+    setSelectedTransaction,
+    totalTransactionsAllOverTime,
+    currentPage,
+    setCurrentPage,
+    lastPage,
+    isLoading,
+}) => {
+    const tableHeaders = ["Date", "Title", "Category", "Amount", "Type", "Actions"];
 
     return (
         <section className="w-full mt-16 font-[inter] px-3 mb-5">
-
-            {totalTransactions == 0 ? <EmptyTransactions /> : <div className="flex overflow-x-auto px-5 rounded-xl py-5 shadow-sm hide-scrollbar">
-                <table className=" min-w-240 w-full ">
-                    <thead>
-                        <tr className="bg-gray-50">
-                            {tableHeaders.map((header) => (
-                                <th key={header} className="px-3 py-4 border-y text-left text-sm font-semibold uppercase tracking-wide  border-gray-200">{header}</th>
-                            ))}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {transactions.length == 0 ?
-                            <NoResult /> :
-                            (
+            {isLoading ? (
+                <TableSkeleton />
+            ) : totalTransactionsAllOverTime === 0 ? (
+                <EmptyTransactions setIsFormOpen={setIsFormOpen} />
+            ) : (
+                <div className="flex overflow-x-auto px-5 rounded-xl py-5 shadow-sm hide-scrollbar">
+                    <table className=" min-w-240 w-full ">
+                        <thead>
+                            <tr className="bg-gray-50">
+                                {tableHeaders.map((header) => (
+                                    <th
+                                        key={header}
+                                        className="px-3 py-4 border-y text-left text-sm font-semibold uppercase tracking-wide  border-gray-200"
+                                    >
+                                        {header}
+                                    </th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {totalTransactions === 0 ? (
+                                <NoResult />
+                            ) : (
                                 transactions.map((transaction) => (
-                                    <TransactionRow key={transaction._id}
+                                    <TransactionRow
+                                        key={transaction._id}
                                         transaction={transaction}
                                         setEditingTransaction={setEditingTransaction}
                                         setIsFormOpen={setIsFormOpen}
@@ -39,14 +55,19 @@ const TransactionTable = ({ transactions, totalTransactions, setIsFormOpen, setE
                                         setSelectedTransaction={setSelectedTransaction}
                                     />
                                 ))
-                            )
-                        }
-                    </tbody>
-                </table>
-            </div>}
-
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+            )}
+            <Pagination
+                setCurrentPage={setCurrentPage}
+                currentPage={currentPage}
+                lastPage={lastPage}
+            />
+            <Footer />
         </section>
-    )
-}
+    );
+};
 
-export default TransactionTable
+export default TransactionTable;
