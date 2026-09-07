72d81efc39d9f6bd6e260da5f9d30d50cfcccc23
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-09-07 16:32:26 +0530

    refactor: configure CORS origin with environment variable

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,15 @@
 import express from "express";
 import authRoutes from "./modules/auth/auth.routes.js";
 import cors from "cors";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import transactionRoutes from "./modules/transaction/transaction.route.js";
-
+dotenv.config();
 const app = express();
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: process.env.FRONTEND_URL,
         credentials: true,
     }),
 );
